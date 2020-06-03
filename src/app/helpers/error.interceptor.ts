b09a5aef50412d89ed6b1522bfed1376e9427121
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-06-03 20:47:10 -0300

    agrego condicion para evitar error en error interceptor

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -6,21 +6,22 @@ import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          // auto logout if 401 response returned from api
+          this.authenticationService.logout();
+        }
 
-            const error = err.error.message || err.statusText;
-            // TODO: agregar acá una llamada al error toast que muestre un error bonito al usuario
-            // TODO: acá tambien podemos agregar el spinner, dejo guardado algo que encontré para armarlo
-            // https://medium.com/@arunkumarv/angular-spinner-with-http-interceptor-d829a5e84921
-            return throwError(error);
-        }));
-    }
+        const error = (err.error && err.error.message) || err.statusText;
+        // TODO: agregar acá una llamada al error toast que muestre un error bonito al usuario
+        // TODO: acá tambien podemos agregar el spinner, dejo guardado algo que encontré para armarlo
+        // https://medium.com/@arunkumarv/angular-spinner-with-http-interceptor-d829a5e84921
+        return throwError(error);
+      })
+    );
+  }
 }
