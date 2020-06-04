7261a67a9baa27f3e94e19bde14af10299a41302
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-06-03 21:17:48 -0300

    habilito alerts para en caso de error

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -3,10 +3,11 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthenticationService } from '../services/authentication.service';
+import { AlertService } from '../services/alert.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService, private alertService: AlertService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
@@ -15,11 +16,8 @@ export class ErrorInterceptor implements HttpInterceptor {
           // auto logout if 401 response returned from api
           this.authenticationService.logout();
         }
-
         const error = (err.error && err.error.message) || err.statusText;
-        // TODO: agregar acá una llamada al error toast que muestre un error bonito al usuario
-        // TODO: acá tambien podemos agregar el spinner, dejo guardado algo que encontré para armarlo
-        // https://medium.com/@arunkumarv/angular-spinner-with-http-interceptor-d829a5e84921
+        this.alertService.error(error);
         return throwError(error);
       })
     );
