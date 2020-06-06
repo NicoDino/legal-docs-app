e3dddbeafee80c02602727bd38fe3a473b6bb870
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-06-06 17:50:27 -0300

    uso jwt helper en authguard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    constructor(
-        private router: Router
-    ) { }
+  constructor(
+    private authService: AuthenticationService,
+    private jwtHelper: JwtHelperService
+  ) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('currentUser')) {
-            return true;
-        }
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login']);
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const token = this.authService.getToken();
+
+    if (this.jwtHelper.isTokenExpired(token)) {
+      // not logged in so logging out
+      this.authService.logout();
+      return false;
     }
+    return true;
+  }
 }
