61275c83bb1f323a3a967dd836e476fcd9a4e8dd
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-06-06 17:49:15 -0300

    instalo jwt helper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,43 +6,40 @@ import { AppComponent } from './app.component';
 import { AlertComponent } from './components/alert.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { ErrorInterceptor } from './helpers/error.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { routing } from './app.routing';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { ArbolItemComponent } from './categorias/arbol-item/arbol-item.component';
 import { CrearCategoriaComponent } from './categorias/crear-categoria/crear-categoria.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { AuthInterceptor } from './helpers/auth.interceptor';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { FaqsComponent } from './faqs/faqs.component';
 import { CrearFaqComponent } from './faqs/crear-faq/crear-faq.component';
-import { EditorModule } from "@tinymce/tinymce-angular";
+import { EditorModule } from '@tinymce/tinymce-angular';
+import { ConfiguracionUsuarioComponent } from './usuarios/configuracion-usuario/configuracion-usuario.component';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        routing,
-        FormsModule,
-        EditorModule
-    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        MenuComponent,
-        HomeComponent,
-        LoginComponent,
-        CategoriasComponent,
-        FaqsComponent,
-        ArbolItemComponent,
-        CrearCategoriaComponent,
-        CrearFaqComponent
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    ],
-    bootstrap: [AppComponent]
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule, routing, FormsModule, EditorModule],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent,
+    CategoriasComponent,
+    FaqsComponent,
+    ArbolItemComponent,
+    CrearCategoriaComponent,
+    CrearFaqComponent,
+    ConfiguracionUsuarioComponent,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+  ],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
