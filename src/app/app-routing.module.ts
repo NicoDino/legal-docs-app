703f8223a55b0f2a7f6f019f4d9015f8b81ee240
Nicolas Dinolfo <nicolas.dinolfio@xpo.com>
2020-06-09 21:10:32 -0300

    agrego guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,9 @@ import { CrearFaqComponent } from './faqs/crear-faq/crear-faq.component';
 import { ConfiguracionUsuarioComponent } from './usuarios/configuracion-usuario/configuracion-usuario.component';
 import { NgModule } from '@angular/core';
 import { ReestablecerPassComponent } from './reestablecer-pass/reestablecer-pass.component';
+import { DocumentosComponent } from './documentos/documentos.component';
+import { CrearCampoComponent } from './documentos/campos/crear-campo/crear-campo.component';
+import { CrearDocumentoComponent } from './documentos/crear-documento/crear-documento.component';
 
 const appRoutes: Routes = [
   /* RUTAS PRIVADAS */
@@ -18,6 +21,9 @@ const appRoutes: Routes = [
   { path: 'faqs', component: FaqsComponent, canActivate: [AuthGuard] },
   { path: 'crear-faq', component: CrearFaqComponent, canActivate: [AuthGuard] },
   { path: 'app-configuracion-usuario', component: ConfiguracionUsuarioComponent, canActivate: [AuthGuard] },
+  { path: 'documentos', component: DocumentosComponent , canActivate: [AuthGuard] },
+  { path: 'crear-documento', component: CrearDocumentoComponent, canActivate: [AuthGuard] },
+  { path: 'crear-campo/:idDocumento', component: CrearCampoComponent , canActivate: [AuthGuard] },
   /*  RUTAS PUBLICAS  */
   { path: 'login', component: LoginComponent },
   { path: 'app-reestablecer-pass', component: ReestablecerPassComponent },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,50 @@ import { AuthenticationService } from '../services/authentication.service';
 import { User } from '../models/user';
 import { Documento } from '../models/documento';
 import { DocumentosService } from '../services/documentos.service';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
 
 @Component({
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  currentUser: User;
+export class HomeComponent implements OnInit, OnDestroy {
+    currentUser: User;
+    currentUserSubscription: Subscription;
+    users: User[] = [];
+    documentos: Documento[];
 
-  constructor(private authenticationService: AuthenticationService) {}
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private documentoService: DocumentosService
+    ) {
+        this.currentUserSubscription = this.authenticationService.currentUser$.subscribe(user => {
+            this.currentUser = user;
+        });
+    }
 
-  ngOnInit() {
-    this.currentUser = this.authenticationService.currentUserValue;
-  }
+    ngOnInit() {
+        this.loadAllUsers();
+        this.loadAllDocumentos();
+    }
+
+    ngOnDestroy() {
+        // unsubscribe to ensure no memory leaks
+        this.currentUserSubscription.unsubscribe();
+    }
+
+
+    private loadAllUsers() {
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.users = users;
+        });
+    }
+
+    private loadAllDocumentos() {
+        this.documentoService.getAll().pipe(first()).subscribe(documentos => {
+            this.documentos = documentos;
+        });
+    }
 }
