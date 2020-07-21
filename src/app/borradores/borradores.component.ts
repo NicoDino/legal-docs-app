ef99d5f59717fbb327a21f6981a021466f499f03
jotero <jotero@jotero>
2020-07-20 23:13:11 -0300

    feat: filtros borradores

--- src/app/borradores/borradores.component.ts
+++ src/app/borradores/borradores.component.ts
@@ -9,8 +9,15 @@ import { Borrador } from '../models/borrador';
   templateUrl: './borradores.component.html',
 })
 export class BorradoresComponent implements OnInit {
+
   $borradores: Observable<Borrador[]> = new Observable<Borrador[]>();
-  constructor(private borradoresService: BorradoresService) {}
+  public desde;
+  public hasta;
+  public email;
+  public borradores;
+  public enviando = false;
+
+  constructor(private borradoresService: BorradoresService) { }
 
   ngOnInit(): void {
     this.getBorradores();
@@ -18,11 +25,36 @@ export class BorradoresComponent implements OnInit {
 
   private getBorradores() {
     this.$borradores = this.borradoresService.getAll();
+    this.filtrar();
   }
 
   reenviar(borrador) {
+    this.enviando = true;
     this.borradoresService.reenviar(borrador).subscribe((res) => {
       alert('Documento reenviado!');
+      this.enviando = false;
+    },
+      () => {
+        this.enviando = false;
+      });
+  }
+
+  filtrar() {
+    this.$borradores.subscribe(resultado => {
+      this.borradores = resultado;
+      if (this.desde) {
+        this.borradores = this.borradores.filter(element =>
+          element.createdAt >= this.desde)
+      }
+      if (this.hasta) {
+        this.borradores = this.borradores.filter(element =>
+          element.createdAt <= this.hasta)
+      }
+      if (this.email) {
+        this.borradores = this.borradores.filter((element: any) => {
+          return element.emailCliente.includes(this.email);
+        });
+      };
     });
   }
 }
